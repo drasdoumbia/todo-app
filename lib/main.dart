efdb25b40a755b78b1d5a5f51c16d54632daadd3
drasdoumbia <subscript.d@gmail.com>
2021-02-04 11:47:59 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(
+        child: Text("Networking with Flutter"),
+      ),
+    );
+  }
+}
